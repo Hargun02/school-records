ec8686665ef10ec6734e25fca6c0c7a9b40aa3f4
Hargun Singh Tikku <hargunsinghtikku@gmail.com>
2022-06-28 13:36:37 +0530

    feat: add Delhi-specific address class

---
+++ main.js
@@ -0,0 +1,17 @@
+class Address {
+    constructor(num, street, pincode) {
+        this.num = num;
+        this.street = street;
+        this.city = "New Delhi";
+        this.state = "Delhi";
+        this.country = "India";
+        this.pincode = pincode;
+    }
+    address = `${num}, ${street}, ${city}, ${state}, ${country}, ${pincode}`;
+}
+
+
+let school = {
+    name: "Sri Venkateshwar International School",
+    address: new Address("Sri Venkateshwar International School", "Sector 18A, Dwarka", 110075)
+}
