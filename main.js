319c89a281b163030df9d65bb187c43335fdb663
Hargun Singh Tikku <hargunsinghtikku@gmail.com>
2022-07-04 01:43:11 +0530

    feat: add name and student classes;

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ let school = localStorage.getItem('school') ?? {
 }
 
 form = document.forms["studentData"]
+
+class Name {
+    constructor(firstName, lastName, middleName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName ?? '';
+    }
+}
+
+class Student {
+    constructor(name, birthDate, grade) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.grade = grade;
+    }
+    subjects = [];
+}
