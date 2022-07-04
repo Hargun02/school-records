50ccebdb03ca6401d832ff795f140fcf3d984ec8
Hargun Singh Tikku <hargunsinghtikku@gmail.com>
2022-07-04 22:13:19 +0530

    style: add spacing b/w comments and previous code

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let school = JSON.parse(localStorage.getItem('school')) ?? {
 }
 
 form = document.querySelector('#student-data')
+
 // Defining classes
 class Address {
     constructor(num, street, pincode) {
@@ -40,6 +41,7 @@ class Student {
     }
     subjects = [];
 }
+
 // Adding references to form elements
 const firstName = form.firstName;
 const lastName = form.lastName;
@@ -56,10 +58,12 @@ const grade = form.grade;
 
 const thead = document.querySelector('#records-head');
 const tbody = document.querySelector('#records-body')
+
 // Getting values from form
 function fetchValues() {
     return new Student(new Name(firstName.value, lastName.value, middleName.value), birthDate.value, grade.value, new Address(houseNum.value, streetName.value, city.value, state.value, pincode.value));
 }
+
 // Updating values to add submitted data
 function updateView() {
     resetTable();
@@ -79,11 +83,13 @@ function onSubmit() {
     school.students.push(fetchValues());
     updateView();
 }
+
 // Event listener on form
 form.addEventListener('submit', event => {
     event.preventDefault();
     onSubmit()
 });
+
 // updateView helper functions
 function createTable() {
     const tr = document.createElement('tr');
