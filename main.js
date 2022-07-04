ab38d0b1ff241fa5d73d2b8046a442534ae74bd8
Hargun Singh Tikku <hargunsinghtikku@gmail.com>
2022-07-04 22:01:06 +0530

    docs: add comments to differentiate regions

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+// Defining the main object
 let school = JSON.parse(localStorage.getItem('school')) ?? {
     name: null,
     students: [],
 }
 
 form = document.querySelector('#student-data')
-
+// Defining classes
 class Address {
     constructor(num, street, pincode) {
         this.num = num;
@@ -39,7 +40,7 @@ class Student {
     }
     subjects = [];
 }
-
+// Adding references to form elements
 const firstName = form.firstName;
 const lastName = form.lastName;
 const middleName = form.middleName;
@@ -55,11 +56,11 @@ const grade = form.grade;
 
 const thead = document.querySelector('#records-head');
 const tbody = document.querySelector('#records-body')
-
+// Getting values from form
 function fetchValues() {
     return new Student(new Name(firstName.value, lastName.value, middleName.value), birthDate.value, grade.value, new Address(houseNum.value, streetName.value, city.value, state.value, pincode.value));
 }
-
+// Updating values to add submitted data
 function updateView() {
     createTable();
     for(let student in school.students) {
@@ -77,12 +78,12 @@ function onSubmit() {
     school.students.push(fetchValues());
     updateView();
 }
-
+// Event listener on form
 form.addEventListener('submit', event => {
     event.preventDefault();
     onSubmit()
 });
-
+// updateView helper functions
 function createTable() {
     const tr = document.createElement('tr');
     tr.appendChild(createHeading("Name"));
